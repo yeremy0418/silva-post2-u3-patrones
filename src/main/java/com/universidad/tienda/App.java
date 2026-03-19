d55f0b2f3160838dc2d6b2315f1bfa392d268091
yeremy0418 <145410510+yeremy0418@users.noreply.github.com>
2026-03-19 16:40:42 -0500

    feat: agregar servicio de armado y demo de orden decorada

--- src/main/java/com/universidad/tienda/App.java
+++ src/main/java/com/universidad/tienda/App.java
@@ -1,13 +1,25 @@
 package com.universidad.tienda;
 
+import com.universidad.tienda.decorator.Orden;
+import com.universidad.tienda.decorator.OrdenServicio;
+
 /**
- * Hello world!
+ * Demostracion basica del patron Decorator para ordenes.
  *
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+public class App {
+    public static void main(String[] args) {
+        OrdenServicio servicio = new OrdenServicio();
+
+        Orden ordenPremium = servicio.crearOrdenCompletaConDescuento(
+                "Laptop",
+                1200.0,
+                true,
+                true,
+                120.0
+        );
+
+        System.out.println("Descripcion: " + ordenPremium.getDescripcion());
+        System.out.println("Costo final: $" + ordenPremium.getCosto());
     }
 }
