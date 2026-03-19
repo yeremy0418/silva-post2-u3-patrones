4971c46bfe0f02e4a812a4bd2d18d45d5edb4799
yeremy0418 <145410510+yeremy0418@users.noreply.github.com>
2026-03-19 16:32:14 -0500

    Agregar archivos iniciales del proyecto: pom.xml, App.java y AppTest.java

---
+++ src/main/java/com/universidad/tienda/App.java
@@ -0,0 +1,13 @@
+package com.universidad.tienda;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
